a911d84e58879a5a890abd41f199750c5cebf6d1
NataliaWiman <natalia.wiman@gmail.com>
2020-06-12 21:12:35 +0200

    Исправляет альтернативный текст к изображению карточки

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -8,7 +8,7 @@ const Main = ({title, genre, date, titlesList}) => {
       return (
         <article className="small-movie-card catalog__movies-card" key={index}>
           <div className="small-movie-card__image">
-            <img src="img/war-of-the-worlds.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+            <img src="img/war-of-the-worlds.jpg" alt={item} width="280" height="175" />
           </div>
           <h3 className="small-movie-card__title">
             <a className="small-movie-card__link" href="movie-page.html">{item}</a>
